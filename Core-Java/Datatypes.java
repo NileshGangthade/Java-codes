2198b9358d032c6ec1a8ba9d56b2a2e211009dcd
Nilesh Gangthade <nileshgangthade7171@gmail.com>
2023-06-18 09:00:19 +0530

    Strings methods

--- Core-Java/Datatypes.java
+++ Core-Java/Datatypes.java
@@ -3,6 +3,7 @@ public class Datatypes {
         System.out.println("int min: "+ Integer.MIN_VALUE);
         System.out.println("int max: "+ Integer.MAX_VALUE);
         System.out.println("int byts: "+ Integer.BYTES);
+        System.out.println('a'+1);
 
         
 
---
+++ Core-Java/Strings_Operatation.java
@@ -0,0 +1,42 @@
+public class Strings_Operatation {
+    public static void main(String[] args) {
+        String name = "This is a string operation code for a string operation.";
+        System.out.println(name);
+        System.out.println(name.length());
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        // System.out.println(name.charAt(2));
+        // System.out.println(name.charAt(3));
+        // System.out.println(name.charAt(4));
+        // System.out.println(name.charAt(5));
+        // System.out.println(name.charAt(6));
+        // System.out.println(name.charAt(7));
+        // System.out.println(name.charAt(8));
+        // System.out.println(name.charAt(9));
+        // System.out.println(name.charAt(10));
+        // System.out.println(name.charAt(11));
+        // System.out.println(name.charAt(12)); 
+        System.out.println(name.toCharArray());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.trim());
+        System.out.println(name.replace("This", "This is"));
+        System.out.println(name.replaceAll("This", "This is"));
+        System.out.println(name.replaceFirst("This", "This is"));
+        System.out.println(name.contains("This"));
+        System.out.println(name.startsWith("This"));
+        System.out.println(name.endsWith("This"));
+        System.out.println(name.indexOf("This"));
+        System.out.println(name.lastIndexOf("This"));
+        System.out.println(name.indexOf("This", 10));
+        System.out.println(name.lastIndexOf("This", 10));
+        System.out.println(name.indexOf("This", -1));
+        System.out.println(name.lastIndexOf("This", -1));
+        System.out.println(name.isEmpty());
+        System.out.println(name.length());
+        System.out.println(name.substring(4));
+        System.out.println(name.substring(4, 10));
+        System.out.println(name.replace('t', ' '));
+
+    }
+}
