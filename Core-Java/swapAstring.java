ab0365fe11557261237152322be3c84674398ba1
Nilesh Gangthade <nileshgangthade7171@gmail.com>
2023-07-04 19:15:32 +0530

    String Character  Swapping

---
+++ Core-Java/swapAstring.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class swapAstring {
+    public static boolean swapAstring(String s, String goal){
+        char c = s.charAt(0);
+        char b = s.charAt(1);
+        char a []={b,c};
+        String n= new String(a);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(n);
+        if (n.charAt(0)==goal.charAt(0) && n.charAt(1)==goal.charAt(1)) {
+            return true;
+        }
+
+        else{
+
+            return false;
+        } 
+        
+    }
+    
+}
+class swap{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a 1st string : ");
+        String s= sc.next();
+        System.out.println("Enter a 2st string : ");
+        String goal= sc.next();
+        swapAstring a=new swapAstring();
+        System.out.println(a.swapAstring(s, goal));
+        sc.close();
+    }
+}
